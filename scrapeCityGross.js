795c797522b8786cadc2b924d2638d0863b9b1ba
Linus Igelström <linusigelstrom@gmail.com>
2025-07-19 20:14:56 +0200

    changed targeted scrape classes, cleanup and reorganize

--- scrapeCityGross.js
+++ scrapeCityGross.js
@@ -37,7 +37,26 @@ async function scrapeCityGross() {
     items.map((item) => {
       const name = item.querySelector("h3")?.innerText.trim() ?? null;
       const volume = item.querySelector("p")?.innerText.trim() ?? null;
-      const price = item.querySelector(".sc-eVZGIO")?.innerText.trim() ?? null;
+
+      let price = item.querySelector("div.sc-islFiG")?.innerText.trim() ?? null;
+      if (price) {
+        const match = price.match(/.*pris.*$/i);
+        price = match ? match[0].trim() : price.trim();
+        price = price.replace(/^pris\s*/i, "");
+      }
+
+      let priceMultipleItems =
+        item.querySelector("div.sc-kSGOQU")?.innerText.trim() ?? null;
+      if (priceMultipleItems) {
+        priceMultipleItems = priceMultipleItems.replace(/^kampanj\s*/i, "");
+        priceMultipleItems = priceMultipleItems.replace(/^klipp!\s*/i, "");
+        priceMultipleItems = priceMultipleItems.replace(/^PRIO-pris\s*/i, "");
+      }
+
+      let compareOrdinaryPrice =
+        item
+          .querySelector(".push-to-bottom")
+          ?.innerText.replace(/[\n\r\t\\]/g, "") ?? null;
 
       const img = item.querySelector("img");
       let imageURL = null;
@@ -63,31 +82,14 @@ async function scrapeCityGross() {
         }
       }
 
-      let getMorePrice =
-        item.querySelector(".sc-cLNonn")?.innerText.trim() ?? null;
-      const blacklist = ["kampanj", "prio-pris", "klipp!"];
-      if (
-        getMorePrice &&
-        blacklist.some((word) =>
-          getMorePrice.toLowerCase().includes(word.toLowerCase())
-        )
-      ) {
-        getMorePrice = null;
-      }
-
-      const compareOrdinaryPrice =
-        item
-          .querySelector(".push-to-bottom")
-          ?.innerText.replace(/[\n\r\t\\]/g, "") ?? null;
-
       return {
         name,
         price,
         store: "CityGross",
         volume,
-        getMorePrice,
         compareOrdinaryPrice,
         imageURL,
+        priceMultipleItems,
       };
     })
   );
@@ -97,9 +99,9 @@ async function scrapeCityGross() {
   // products.forEach((product, i) => {
   //   console.log(`\nProdukt ${i + 1}:`);
   //   console.log("Namn:", product.name);
+  //   console.log("Pris för fler:", product.priceMultipleItems);
   //   console.log("Pris:", product.price);
   //   console.log("Volym:", product.volume);
-  //   console.log("Kampanjpris:", product.getMorePrice);
   //   console.log("Jämförpris:", product.compareOrdinaryPrice);
   //   console.log("Bild:", product.imageURL);
   // });
